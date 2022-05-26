e2b70f0447326a2578c7cfa7f49b4f6d4162ca36
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-05-26 20:03:54 +0200

    install axios and graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 const Home: NextPage = () => {
-  return (
-    <div>
+  console.log(process.env.WORDPRESS_API_URL);
+  return <div>asd</div>;
+};
 
-    </div>
-  )
-}
-
-export default Home
+export default Home;
