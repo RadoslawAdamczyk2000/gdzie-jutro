cd504f85dc2a98d7f7243799e48b108ccf56c5dd
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-05-27 09:06:05 +0200

    download logo image

--- lib/wordpress/api.ts
+++ lib/wordpress/api.ts
@@ -1,15 +1,18 @@
 //  20 posts
-query allPosts {
-    posts(first: 20, where: {orderby: {field: DATE, order: DESC}}) {
-      nodes {
-        date
-        id
-        title
-        excerpt
-        slug
-      }
+
+export const ALL_POSTS = `
+    query allPosts {
+        posts {
+            nodes {
+                date
+                id
+                title
+                excerpt
+                slug
+            }
+        }
     }
-  }
+`
 //  ssr
 
 //  post by a slug
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from "next";
-const Home: NextPage = () => {
-  console.log(process.env.WORDPRESS_API_URL);
-  return <div>asd</div>;
+import fetcher from "../lib/wordpress/fetcher";
+import { ALL_POSTS } from "../lib/wordpress/api";
+
+const Home:NextPage = ({allPosts}:{allPosts:object}) => {
+  const posts = allPosts;
+  console.log(posts);
+  return( 
+    <div>
+      {posts.slice(0,5).map(({excerpt,title}) =>
+        <div>
+          <h2>{title}</h2>
+          <p dangerouslySetInnerHTML={{__html:excerpt}}/>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const response = await fetcher(ALL_POSTS);
+  const allPosts = response.data.posts.nodes;
+  return{
+    props:{allPosts},
+    revalidate:1,
+  }
+}
